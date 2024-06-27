ff571327166ed05e42cbf99ca5a34877a2cee20b
Ismael <ismaelmartin302@gmail.com>
2024-06-27 11:53:48 +0200

    cambiando todo, ahora hago ejercicios

--- Main.java
+++ Main.java
@@ -1,53 +1,15 @@
 
-import java.util.Scanner;
-
+import ejercicios.ejercicios;
 
 public class Main {
+    
     public static void main(String[] args) {
-        Main main = new Main();
-        Integer x = 0, y = 0, resultado = null;
-        Scanner myScanner = new Scanner(System.in);
-        System.out.println("X= ");
-        x = myScanner.nextInt();
-        System.out.println("Y= ");
-        y = myScanner.nextInt();
-        myScanner.nextLine();
-        System.out.println("Operación? ");
-        String operacion = myScanner.nextLine().toLowerCase();
-        switch (operacion) {
-            case "suma":
-                resultado = main.suma(x, y);
-                break;
-            case "resta":
-                resultado = main.resta(x, y);
-                break;
-            case "multiplicacion":
-                resultado = main.multiplicacion(x, y);
-                break;
-            case "division":
-                resultado = main.division(x, y);
-                break;
-            default:
-                System.out.println("Operacion no valida");
-                break;
-        }
-        myScanner.close();
-
-        System.err.println(resultado);
-        
+        System.out.println("\r\n -------Iniciando código------- \n");
+        System.out.println("\nSerie Numerica utilizando For");
+        ejercicios.serieNumericaFor();
+        System.out.println("\nSerie Numerica utilizando While");
+        ejercicios.serieNumericaWhile();
+        System.out.println("\nSerie Numerica utilizando Do While");
+        ejercicios.serieNumericaDoWhile();
     }
-
-    private int suma(int x, int y) {
-        return x + y;
-    }
-    public int resta(int x, int y) {
-        return x - y;
-    }
-    public int multiplicacion(int x, int y) {
-        return x * y;
-    }
-    public int division(int x, int y) {
-        return x / y;
-    }
-
 }
---
+++ ejercicios/ejercicios.java
@@ -0,0 +1,23 @@
+package ejercicios;
+
+public class ejercicios {
+    public static void serieNumericaFor () {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+    public static void serieNumericaWhile () {
+        int i = 1;
+        while (i <= 10) {
+            System.out.println(i);
+            i++;
+        }
+    }
+    public static void serieNumericaDoWhile() {
+        int i = 1;
+        do {
+            System.out.println(i);
+            i++;
+        } while (i <= 10);
+    }
+}
